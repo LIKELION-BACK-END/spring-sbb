cf7a3f08d5280a541a47e72c3befa452aad0769c
zzerowin <alaykr123@gmail.com>
2022-08-10 11:40:36 +0900

    Answer, Question 클래스 추가

---
+++ src/main/java/com/ll/exam/ssb/Question.java
@@ -0,0 +1,26 @@
+package com.ll.exam.ssb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
