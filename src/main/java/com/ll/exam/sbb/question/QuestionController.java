6c3e6620e99a1587756b86c9cda9ed415db06838
zzerowin <alaykr123@gmail.com>
2022-08-16 11:25:02 +0900

    질문 리스트 템플릿 작업

---
+++ src/main/java/com/ll/exam/sbb/question/QuestionController.java
@@ -0,0 +1,29 @@
+package com.ll.exam.sbb.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor // 생성자 주입
+public class QuestionController {
+    // @Autowired // 필드 주입
+    private final QuestionService questionService;
+
+    @RequestMapping("/question/list")
+    // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+
+        // 미래에 실행된 question_list.html 에서
+        // questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
+        model.addAttribute("questionList", questionList);
+
+        return "question_list";
+    }
+}
